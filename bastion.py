37870e568890ebf8d1bedb1d898adc131e08d5e6
bastionbot <droneship16@gmail.com>
2017-09-18 23:19:41 -0500

    commented out the seeding
    ffs this is annoying

--- bastion.py
+++ bastion.py
@@ -97,7 +97,7 @@ async def on_message(message):
             await client.send_message(message.channel, 'Failed to tweet :saddowns:')
     if message.content.startswith('!markov'):
         try:
-            msg[message.author.id] = markov.sayrandomshit(corpus[0], str(re.sub(r'!\w+\s', '', msgcontent)))
+            msg[message.author.id] = markov.sayrandomshit(corpus[0])#, str(re.sub(r'!\w+\s', '', msgcontent)))
         except:
             msg[message.author.id] = 'Something bad happened and I don\'t know what'
         await client.send_message(message.channel, msg[message.author.id])
