53558289592f9f4662e40abb8b47eff585344594
bastionbot <droneship16@gmail.com>
2018-10-15 20:45:21 -0500

    .

--- bastion.py
+++ bastion.py
@@ -8,36 +8,16 @@ import twitter
 import threading
 import asyncore
 import time
+import configparser
+import atexit
 
-os.chdir('/usr/local/bin/discord')
-with open('twitkeys') as f:
-    lines = f.readlines()
-api = twitter.Api(consumer_key=lines[0].strip(), 
-                  consumer_secret=lines[1].strip(),
-                  access_token_key=lines[2].strip(),
-                  access_token_secret=lines[3].strip())
-with open('oldMention') as f:
-    oldMention = f.readlines()[0].strip()
-f = open("botkey", 'r')
-botkey = str(f.readline())
-f.close()
-corpus, ignoreUsers, ignoreChannels = [""], [], []
-botkey = botkey.rstrip()
+corpus = [""]
 client = discord.Client()
 msg = {}
-welcomeMsgStrings = []
-with open('welcome') as f:
-    welcomeMsgStrings = f.readlines()
-with open("ignores") as f:
-	for line in f:
-		if line.split()[0] == "channel":
-			ignoreChannels.append(line.split()[1])
-		elif line.split()[0] == "user":
-			ignoreUsers.append(line.split()[1])
 
 welcomeMsg1 = "> Welcome to Serious Overchill! "
 welcomeMsg3 = " Check the pins here in <#193536175451930624> on how to get your very own vanity roles for pretty colors and "
-welcomeMsg4 = "LFG pings. Stuck or have questions? Don\'t be afraid to ask a Kabalite or Mod in <#184804980794851328> for help! "
+welcomeMsg4 = "LFG pings. Stuck or have questions? Don\'t be afraid to ask a Mod in <#184804980794851328> for help! "
 welcomeMsg5 = "Brace yourself for a Bastion Thought™: ```"
 
 class mentionHandler(threading.Thread):
@@ -52,11 +32,8 @@ class mentionHandler(threading.Thread):
         global corpus
         while True:
             self.respond = {}
-            with open("oldMention") as f:
-                self.oldMention = f.readlines()[0].strip()
             time.sleep(300)
-            mentions = self.api.GetMentions(since_id=self.oldMention)
-            replies = self.api.GetReplies(since_id=self.oldMention)
+            mentions, replies = self.api.GetMentions(since_id=self.oldMention), self.api.GetReplies(since_id=int(self.oldMention))
             for line in mentions:
                 if line.user.screen_name != "SRSOC_Bastion":
                     self.respond[line.id_str] = line.user.screen_name
@@ -66,35 +43,53 @@ class mentionHandler(threading.Thread):
             if len(self.respond) is 0:
                 continue
             self.oldMention = sorted(self.respond.keys())[-1]
-            wfile = open("oldMention", "w")
-            wfile.write(self.oldMention)
-            wfile.close()
+            config['standard']['oldMention'] = self.oldMention
             for i in self.respond.keys():
                self.api.PostUpdate(status="@"+self.respond[i]+" "+markov.sayrandomtweet(corpus[0]), in_reply_to_status_id=i)
-        
-t = mentionHandler(api, oldMention)
-t.start()
-                     
+
+def start():
+    os.chdir('/usr/local/bin/discord')
+    corpus[0] = markov.buildcorpus()
+    config = configparser.ConfigParser()
+    config.read('config')
+    with open('welcome') as f:
+        welcomeMsgStrings = f.readlines()
+    twitkeys = dict(config['twitter'])
+    api = twitter.Api(**twitkeys)
+    t = mentionHandler(api, str(config['standard']['oldMention']))
+    t.start()
+    return config, corpus, t, welcomeMsgStrings, api
+
+@atexit.register
+def stop():
+    with open('config', 'w') as f:
+        config.write(f)
+
+def writeCorpus(message):
+    writeContent = message.content + '\n'
+    if message.author.id in config['ignoreUsers']['users'].split(',\n'):
+        return
+    if message.content.startswith('!'):
+        return
+    if message.channel.id in config['ignoreChannels']['chans'].split(',\n'):
+        return
+    with open("discord_corpus", "a") as wfile:
+        removeLinks = re.sub(r'http\S+', '', writeContent)
+        removeMentions = re.sub(r'<[@]?[&!]?[\d]*>','', removeLinks)
+        wfile.write(removeMentions)
+
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.send_message(client.get_channel('193536175451930624'), 'Bastionbot restarted')
+    await client.send_message(client.get_channel(config['standard']['botchannel']), 'Bastionbot restarted')
 
 @client.event
 async def on_message(message):
     global msg
-    global welcomeMsgStrings
-    msgcontent = message.content
-    writecontent = msgcontent + '\n'
-    msgauthor = str(message.author)
-    if message.author.id not in ignoreUsers and not message.content.startswith('!') and message.channel.id not in ignoreChannels:
-        wfile = open("discord_corpus", "a")
-        writecontent = re.sub(r'http\S+', '', writecontent)
-        wfile.write(re.sub(r'<[@]?[&!]?[\d]*>','',writecontent))
-        wfile.close()
+    writeCorpus(message)
     if message.content.startswith('!tweet'):
         try:
             tweet = api.PostUpdate(msg[message.author.id])
@@ -104,26 +99,36 @@ async def on_message(message):
             await client.send_message(message.channel, 'Failed to tweet :saddowns:')
     if message.content.startswith('!markov'):
         try:
-            msg[message.author.id] = markov.sayrandomshit(corpus[0])#, str(re.sub(r'!\w+\s', '', msgcontent)))
+            msg[message.author.id] = markov.sayrandomshit(corpus[0], message.content)#, str(re.sub(r'!\w+\s', '', msgcontent)))
         except:
             msg[message.author.id] = 'Something bad happened and I don\'t know what'
         await client.send_message(message.channel, msg[message.author.id])
-    if message.content.startswith('!update') and message.channel.id == '282003155993231360':
+    if message.content.startswith('!update') and message.channel.id == config['standard']['adminchannel']:
         welcomeMsgStrings.append(re.sub(r'!\w+\s', '', message.content))
-        wfile = open("welcome", "a")
-        writecontent = re.sub(r'!\w+\s', '', message.content) + '\n'
-        wfile.write(writecontent)
-        wfile.close()
-        await client.send_message(message.channel, 'Successfully updated the welcome packet with ```' + re.sub(r'!\w+\s', '', message.content) + '```')
-        
+        with open("welcome", "a") as wfile:
+            wfile.write(re.sub(r'!\w+\s', '', message.content) + '\n')
+        await client.send_message(message.channel, 'Successfully updated the welcome lines with ```' + re.sub(r'!\w+\s', '', message.content) + '```')
+    if message.content.startswith('!list') and message.channel.id == config['standard']['adminchannel']:
+        tempmsg = ""
+        for welcome in welcomeMsgStrings:
+            tempmsg += welcome
+        await client.send_message(message.channel, "Current welcome strings:\n"+"```"+tempmsg+"```")
+    if message.content.startswith('!bruce') and message.channel.id == config['standard']['adminchannel']:
+        try:
+            server = client.get_server('184804980794851328')
+           # print(server.get_member(message.content.split(' ')[1]).nick)
+            client.change_nickname(server.get_member(message.content.split(' ')[1]), 'Bruce')
+            await client.send_message(message.channel, message.author.mention + " you've bruced " + server.get_member(message.content.split(" ")[1]).nick)
+        except:
+            await client.send_message(message.channel, message.author.mention + " No dice, friend")
+
 @client.event
 async def on_member_join(member):
     msg = "<@" + str(member.id) + welcomeMsg1 + random.choice(welcomeMsgStrings).strip() + welcomeMsg3 \
-    + welcomeMsg4 + welcomeMsg5 + markov.sayrandomshit(corpus[0]) + "```"
-    await client.send_message(client.get_channel('193536175451930624'), msg)
-    
-corpus[0] = markov.buildcorpus()
+    + welcomeMsg4 + welcomeMsg5 + markov.sayrandomshit(corpus[0], "foo") + "```"
+    await client.send_message(client.get_channel(config['standard']['botchannel']), msg)
+
+config, corpus, t, welcomeMsgStrings, api = start()
 print('Corpus built')
 print('------')
-
-client.run(botkey)
+client.run(config['standard']['botkey'])
