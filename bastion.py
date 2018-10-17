151a12d635e060219b3388805be510295f488004
bastionbot <droneship16@gmail.com>
2018-10-17 09:31:23 -0500

    Update bastion.py

--- bastion.py
+++ bastion.py
@@ -10,13 +10,14 @@ import asyncore
 import time
 import configparser
 import atexit
+import sqlite3
 
 corpus = [""]
 client = discord.Client()
 msg = {}
 
 welcomeMsg1 = "> Welcome to Serious Overchill! "
-welcomeMsg3 = " Check the pins here in <#193536175451930624> on how to get your very own vanity roles for pretty colors and "
+welcomeMsg3 = "Check the pins here in <#193536175451930624> on how to get your very own vanity roles for pretty colors and "
 welcomeMsg4 = "LFG pings. Stuck or have questions? Don\'t be afraid to ask a Mod in <#184804980794851328> for help! "
 welcomeMsg5 = "Brace yourself for a Bastion Thought™: ```"
 
@@ -90,41 +91,11 @@ async def on_ready():
 async def on_message(message):
     global msg
     writeCorpus(message)
-    if message.content.startswith('!tweet'):
-        try:
-            tweet = api.PostUpdate(msg[message.author.id])
-            twitmsg = "https://twitter.com/SRSOC_Bastion/status/"+tweet.id_str
-            await client.send_message(message.channel, twitmsg)
-        except:
-            await client.send_message(message.channel, 'Failed to tweet :saddowns:')
-    if message.content.startswith('!markov'):
-        try:
-            msg[message.author.id] = markov.sayrandomshit(corpus[0], message.content)#, str(re.sub(r'!\w+\s', '', msgcontent)))
-        except:
-            msg[message.author.id] = 'Something bad happened and I don\'t know what'
-        await client.send_message(message.channel, msg[message.author.id])
-    if message.content.startswith('!update') and message.channel.id == config['standard']['adminchannel']:
-        welcomeMsgStrings.append(re.sub(r'!\w+\s', '', message.content))
-        with open("welcome", "a") as wfile:
-            wfile.write(re.sub(r'!\w+\s', '', message.content) + '\n')
-        await client.send_message(message.channel, 'Successfully updated the welcome lines with ```' + re.sub(r'!\w+\s', '', message.content) + '```')
-    if message.content.startswith('!list') and message.channel.id == config['standard']['adminchannel']:
-        tempmsg = ""
-        for welcome in welcomeMsgStrings:
-            tempmsg += welcome
-        await client.send_message(message.channel, "Current welcome strings:\n"+"```"+tempmsg+"```")
-    if message.content.startswith('!bruce') and message.channel.id == config['standard']['adminchannel']:
-        try:
-            server = client.get_server('184804980794851328')
-           # print(server.get_member(message.content.split(' ')[1]).nick)
-            client.change_nickname(server.get_member(message.content.split(' ')[1]), 'Bruce')
-            await client.send_message(message.channel, message.author.mention + " you've bruced " + server.get_member(message.content.split(" ")[1]).nick)
-        except:
-            await client.send_message(message.channel, message.author.mention + " No dice, friend")
+    handler.message(message)
 
 @client.event
 async def on_member_join(member):
-    msg = "<@" + str(member.id) + welcomeMsg1 + random.choice(welcomeMsgStrings).strip() + welcomeMsg3 \
+    msg = "<@" + str(member.id) + welcomeMsg1 + welcomeMsg3 \
     + welcomeMsg4 + welcomeMsg5 + markov.sayrandomshit(corpus[0], "foo") + "```"
     await client.send_message(client.get_channel(config['standard']['botchannel']), msg)
 
