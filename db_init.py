8e8bc4648a133b6605dd0fa70c5e90f3286a5490
bastionbot <droneship16@gmail.com>
2018-10-17 09:45:33 -0500

    Create db_init.py

---
+++ db_init.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+if __name__ == '__main__':
+    conn = sqlite3.connect('bastion.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE chathistory
+        (date text, user text, msg text)''')
+    conn.commit()
+    conn.close()
