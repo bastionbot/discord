4b94c3a0fdb703b67af66677dde2788a99942cb7
Arthur Bressan <arthurbressan2@hotmail.com>
2019-08-20 20:27:17 -0300

    Decide commands

--- commands/misc.py
+++ commands/misc.py
@@ -16,7 +16,8 @@ class Misc(Cog):
         You can specify how many, and what kind of dice to roll with the format XdY.
         X is how many dice (optional, defaults to 1).
         Y is how many sides each die has.
-        For example, `roll 2d6` will roll two six-sided dice.
+        For example:
+        `roll 2d6` will roll two six-sided dice;
         `roll d20` will roll a 20-sided die.
         Pass nothing to roll a 6-sided die.
         """
@@ -34,12 +35,24 @@ class Misc(Cog):
         results = list(map(random.randint, [1] * n_dice, [faces] * n_dice))
         await ctx.send(f'{results}: **{sum(results)}**')
 
-    @command(aliases=['add'])
-    async def decide(self, ctx, *, role_name):
+    @command()
+    async def decide(self, ctx, *, options):
         """
-        Decide.
+        Make the bot decide stuff for you!
+        Separate each option with "or", or end your message with y/n for a "yes", "no" pick.
+        For example:
+        `decide pizza or burger`
+        `decide do I go y/n`
         """
-        pass
+        # We probably need to put in more fun messages but meh for now.
+        if options.endswith('y/n'):
+            await ctx.send(f'**{random.choice(["Yes!", "No!"])}**')
+            return
+        choices = options.split(" or ")
+        if len(choices) == 1:
+            await ctx.send('I need to have at least two options!')
+            return
+        await ctx.send(f'**{random.choice(choices)}**')
 
 
 def setup(bot):
