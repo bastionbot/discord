52e579a92c48358cac92f7ceace4bc0fa7fc2d51
bastionbot <droneship16@gmail.com>
2019-10-23 19:35:24 -0500

    Update gofundme.py
    tabs...

--- parsers/gofundme.py
+++ parsers/gofundme.py
@@ -17,19 +17,19 @@ async def gofundme(url, ctx=None):
     progress = ''.join([x.text for x in html.find_all('h2',{'class':'m-progress-meter-heading'})])
     percent = list(progress.split(' ')[i].strip('$') for i in [0, 3])
     percentage = float('%.1f' %(int(percent[0].replace(',','')) / int(percent[1].replace(',','')) * 100))
-	author = html.find('div', {'class': 'm-campaign-byline-description'}).text
+        author = html.find('div', {'class': 'm-campaign-byline-description'}).text
     for ultag in html.find_all('ul', {'class': 'o-campaign-sidebar-donations'}):
         for litag in ultag.find_all('li'):
             donations.append(litag.text)
     clean = {x[0]: x[1] + ' ' + x[2] for x in [don.split('\xa0') for don in donations] if len(x) ==3}
     embed = discord.Embed(title=author, color=1359922)
-	embed.url = url
-	embed.description = "${} / ${}\n**This fundraiser is {}% there!**\n\nRecent contributors".format(percent[0],percent[1],percentage)
-	embed.thumbnail.url = "https://www.gofundme.com/static/media/DefaultAvatar.65712475de0674c9f775ae3b64e7c69f.svg"
-	embed.set_author(name=name, url=url)
-	for x in list(clean)[0:5]:
-		embed.add_field(name=x, value=clean[x], inline=True)
-	await ctx.send(embed=embed, ctx)
+    embed.url = url
+    embed.description = "${} / ${}\n**This fundraiser is {}% there!**\n\nRecent contributors".format(percent[0],percent[1],percentage)
+    embed.thumbnail.url = "https://www.gofundme.com/static/media/DefaultAvatar.65712475de0674c9f775ae3b64e7c69f.svg"
+    embed.set_author(name=name, url=url)
+    for x in list(clean)[0:5]:
+        embed.add_field(name=x, value=clean[x], inline=True)
+    await ctx.send(embed=embed, ctx)
 
 if __name__ == '__main__':
     asyncio.run(gofundme("https://www.gofundme.com/f/help-ben-finish-college"))
