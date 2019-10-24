44b2f219aab1edb91acfa5082ab481c95549d9e3
bastionbot <droneship16@gmail.com>
2019-10-23 22:24:46 -0500

    Update gofundme.py

--- parsers/gofundme.py
+++ parsers/gofundme.py
@@ -12,6 +12,7 @@ async def gofundme(url, ctx=None):
     #await ctx_print(f"gofundme: {datetime.now().isoformat()}", ctx)
     html = bs(urllib.request.urlopen(url), features="html.parser")
     donations = []
+    clean = []
     name = html.find('h1').text
     progress = ''.join([x.text for x in html.find_all('h2',{'class':'m-progress-meter-heading'})])
     percent = list(progress.split(' ')[i].strip('$') for i in [0, 3])
@@ -20,15 +21,17 @@ async def gofundme(url, ctx=None):
     for ultag in html.find_all('ul', {'class': 'o-campaign-sidebar-donations'}):
         for litag in ultag.find_all('li'):
             donations.append(litag.text)
-    clean = {x[0]: x[1] + ' ' + x[2] for x in [don.split('\xa0') for don in donations] if len(x) ==3}
+    for x in [don.split('\xa0') for don in donations]:
+	    if len(x) == 3:
+            clean.append({"name": x[0], "donation": x[1] + ' ' + x[2]})
     embed = discord.Embed(title=author, color=1359922)
     embed.url = url
     embed.description = "${} / ${}\n**This fundraiser is {}% there!**\n\nRecent contributors".format(percent[0],percent[1],percentage)
     embed.thumbnail.url = "https://www.gofundme.com/static/media/DefaultAvatar.65712475de0674c9f775ae3b64e7c69f.svg"
     embed.set_author(name=name, url=url)
-    for x in list(clean)[0:5]:
-        embed.add_field(name=x, value=clean[x], inline=True)
-    embed.add_field(name="Checked at", value=f"{datetime.now().strftime('%H:%M:%S %a %d %b %Y')}")
+    for x in clean[0:6]:
+        embed.add_field(name=x["name"], value=x["donation"], inline=True)
+    embed.add_field(inline=False, name="Checked at", value=f"{datetime.now().strftime('%H:%M:%S %a %d %b %Y')}")
     await ctx.send(embed=embed)
 
 if __name__ == '__main__':
