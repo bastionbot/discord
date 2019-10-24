f4b5311d15d14ead21be5431ad5435720fb17eef
bastionbot <droneship16@gmail.com>
2019-10-23 19:27:38 -0500

    Update gofundme.py

--- parsers/gofundme.py
+++ parsers/gofundme.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import asyncio
+import asyncio, discord
 
 import bs4 as bs
 
@@ -10,18 +10,27 @@ async def ctx_print(_str, ctx=None):
 
 async def gofundme(url, ctx=None):
     await ctx_print(f"gofundme: {datetime.now().isoformat()}", ctx)
-    if url.split(' ')[-1:].startswith('http'):
-        html = bs(urllib.request.urlopen(url.split(' ')[-1:]))
+    if url.startswith('https://www.gofundme.com/f/'):
+        html = bs(urllib.request.urlopen(url))
     donations = []
     name = html.find('h1').text
     progress = ''.join([x.text for x in html.find_all('h2',{'class':'m-progress-meter-heading'})])
     percent = list(progress.split(' ')[i].strip('$') for i in [0, 3])
-    percentage = float('%.3f' %(int(percent[0].replace(',','')) / int(percent[1].replace(',','')) * 100))
+    percentage = float('%.1f' %(int(percent[0].replace(',','')) / int(percent[1].replace(',','')) * 100))
+	author = html.find('div', {'class': 'm-campaign-byline-description'}).text
     for ultag in html.find_all('ul', {'class': 'o-campaign-sidebar-donations'}):
         for litag in ultag.find_all('li'):
             donations.append(litag.text)
-    clean = {x[0]: x[1] for x in [don.split('\xa0') for don in donations] if len(x) ==3}
-    await ctx_print(clean, ctx)
+    clean = {x[0]: x[1] + ' ' + x[2] for x in [don.split('\xa0') for don in donations] if len(x) ==3}
+    embed = discord.Embed(title=author, color=1359922)
+	embed.url = url
+	embed.description = "${} / ${}\n**This fundraiser is {}% there!**\n\nRecent contributors".format(percent[0],percent[1],percentage)
+	embed.thumbnail.url = "https://www.gofundme.com/static/media/DefaultAvatar.65712475de0674c9f775ae3b64e7c69f.svg"
+	embed.author.name = name
+	embed.author.url = url
+	for x in list(clean)[0:5]:
+		embed.add_field(name=x, value=clean[x], inline=True)
+	await ctx.send(embed=embed, ctx)
 
 if __name__ == '__main__':
     asyncio.run(gofundme("https://www.gofundme.com/f/help-ben-finish-college"))
