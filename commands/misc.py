2456297c3c10597073744fc5a92aa33738def485
bastionbot <droneship16@gmail.com>
2019-10-30 07:48:23 -0500

    Update misc.py
    Added 8ball

--- commands/misc.py
+++ commands/misc.py
@@ -6,6 +6,33 @@ class Misc(Cog):
     """
     Miscellaneous fun commands.
     """
+    
+    @command()
+    async def 8ball(self, ctx, question):
+        """
+        Randomly returns one of the 20 original answers in a Magic 8-Ball
+        """
+        answers = [ 'As I see it, yes.', 
+                   'Ask again later.', 
+                   'Better not tell you now.', 
+                   'Cannot predict now.', 
+                   'Concentrate and ask again.', 
+                   'Don’t count on it.', 
+                   'It is certain.', 
+                   'It is decidedly so.', 
+                   'Most likely.', 
+                   'My reply is no.', 
+                   'My sources say no.', 
+                   'Outlook not so good.', 
+                   'Outlook good.', 
+                   'Reply hazy, try again.', 
+                   'Signs point to yes.', 
+                   'Very doubtful.', 
+                   'Without a doubt.', 
+                   'Yes.', 
+                   'Yes – definitely.', 
+                   'You may rely on it.']
+        await ctx.send(f'**{random.choice(answers)}**')
 
     @command()
     async def roll(self, ctx, dice=None):
