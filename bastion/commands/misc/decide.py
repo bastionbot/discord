eb49305ccb4d18c2e3a95d95ab39c32065d036df
Arthur Bressan <arthurbressan2@hotmail.com>
2019-11-02 14:48:21 -0300

    Start implementing decide command

---
+++ bastion/commands/misc/decide.py
@@ -0,0 +1,45 @@
+POSITIVE_CHOICES = [
+    'Yes',
+    'Yeah',
+    'Sure',
+    'This is Chill and Good',
+    'Absolutely',
+    'Do iiiiit OwO',
+    'Hell yeah, mother humper!',
+    'Yeah sure, why not.',
+    'Fuck yeah!',
+    'Yeah, why not.',
+]
+
+NEGATIVE_CHOICES = [
+    'No',
+    'Nah',
+    'nOwO',
+    "No fuckin' way, man.",
+    'No. No! Absolutely not!',
+    'Fuck no!',
+    'OwO not today!!!',
+    'Probably not.',
+    "That's not very cash money.",
+    "Yeah, that's gonna be a no from me, dog."
+]
+
+
+THING_CHOICES = [
+    '{thing}, now get out there and put those pucks in the net',
+    "DO IT!! JUST DO IT!! Don't let your {thing} dreams be dreams",
+    '*nuzzles ur {thing}*',
+    'Your momma so weird she wrote {thing} on her bucket list.',
+    'The world must {thing}',
+    '{thing} fucks. Get up in them guts.',
+    'The high council has concluded that {thing} is it, chief. ',
+    '{thing} might be good, yeah.',
+    '{thing} is fucked up. Hell yeah.',
+    "{thing} S'es Ds",
+    "Check out my huge {thing}. Isn't it gross?",
+    'Duh, {thing}',
+    'Obviously {thing}',
+    'Definitely {thing}',
+    'Verily, I counsel thee for {thing}',
+    '{thing} smothered in sausage gravy, with a side of hashbrowns, and slices of bacon.',
+]
