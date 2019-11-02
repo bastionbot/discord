6a6c2a44d91419f5f32352f5d384ca78c35aee44
Arthur Bressan <arthurbressan2@hotmail.com>
2019-11-02 13:57:02 -0300

    Adds 8ball tests

--- bastion/tests/commands/misc/test_eight_ball.py
+++ bastion/tests/commands/misc/test_eight_ball.py
@@ -1,2 +1,13 @@
-def test_picked_up():
-    assert True == False
+import pytest
+
+from bastion.commands.misc import eight_ball
+
+def test_truthy_question():
+    answer = eight_ball.get_answer('ABC')
+    assert answer in eight_ball.CHOICES
+
+
+def test_falsey_questions():
+    with pytest.raises(ValueError):
+        eight_ball.get_answer(None)
+        eight_ball.get_answer('')
