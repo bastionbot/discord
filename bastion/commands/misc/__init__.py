c5dafa5af35cc48be06d7bdb2b20bf5fb9b4bfa6
Arthur Bressan <arthurbressan2@hotmail.com>
2019-11-02 13:49:08 -0300

    8ball

--- bastion/commands/misc/__init__.py
+++ bastion/commands/misc/__init__.py
@@ -2,40 +2,24 @@ import random
 from discord import utils, Embed
 from discord.ext.commands import Cog, command, group
 
+from bastion.commands.misc import eight_ball
+
 class Misc(Cog):
     """
     Miscellaneous fun commands.
     """
-    
+
     @command(aliases=['8ball'])
     async def eightball(self, ctx, question=None):
         """
         Randomly returns one of the 20 original answers in a Magic 8-Ball
         """
-        answers = [ 'As I see it, yes.', 
-                   'Ask again later.', 
-                   'Better not tell you now.', 
-                   'Cannot predict now.', 
-                   'Concentrate and ask again.', 
-                   'Don’t count on it.', 
-                   'It is certain.', 
-                   'It is decidedly so.', 
-                   'Most likely.', 
-                   'My reply is no.', 
-                   'My sources say no.', 
-                   'Outlook not so good.', 
-                   'Outlook good.', 
-                   'Reply hazy, try again.', 
-                   'Signs point to yes.', 
-                   'Very doubtful.', 
-                   'Without a doubt.', 
-                   'Yes.', 
-                   'Yes – definitely.', 
-                   'You may rely on it.']
-        if question is None:
-            await ctx.send(f'An answer without a question is meaningless.')
+        try:
+            answer = eight_ball.get_answer(question)
+        except ValueError as error:
+            await ctx.send(str(error))
         else:
-            await ctx.send(f'**{random.choice(answers)}**')
+            await ctx.send(f'**{answer}**')
 
     @command()
     async def roll(self, ctx, dice=None):
---
+++ bastion/tests/commands/misc/test_eight_ball.py
@@ -0,0 +1,2 @@
+def test_picked_up():
+    assert True == False
