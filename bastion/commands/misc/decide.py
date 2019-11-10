c376cf3a83656c4741ac483e2c228b4c12feae1d
Arthur Bressan <arthurbressan2@hotmail.com>
2019-11-10 14:38:02 -0300

    Bolds the option and fixes some grammar

--- bastion/commands/misc/decide.py
+++ bastion/commands/misc/decide.py
@@ -1,22 +1,22 @@
 import random
 
 CHOICES = [
-    '{thing}, now get out there and put those pucks in the net',
-    "DO IT!! JUST DO IT!! Don't let your {thing} dreams be dreams",
-    '*nuzzles ur {thing}*',
-    'Your momma so weird she wrote {thing} on her bucket list.',
-    'The world must {thing}',
-    '{thing} fucks. Get up in them guts.',
-    'The high council has concluded that {thing} is it, chief.',
-    '{thing} might be good, yeah.',
-    '{thing} is fucked up. Hell yeah.',
-    "{thing} S'es Ds",
-    "Check out my huge {thing}. Isn't it gross?",
-    'Duh, {thing}',
-    'Obviously {thing}',
-    'Definitely {thing}',
-    'Verily, I counsel thee for {thing}',
-    '{thing} smothered in sausage gravy, with a side of hashbrowns, and slices of bacon.',
+    '**{thing}**, now get out there and put those pucks in the net.',
+    "DO IT!! JUST DO IT!! Don't let your **{thing}** dreams be dreams.",
+    '\\*nuzzles ur **{thing}**\\*',
+    'Your momma so weird she wrote **{thing}** on her bucket list.',
+    'The world must **{thing}**.',
+    '**{thing}** fucks. Get up in them guts.',
+    'The high council has concluded that **{thing}** is it, chief.',
+    '**{thing}** might be good, yeah.',
+    '**{thing}** is fucked up. Hell yeah.',
+    "**{thing}** S'es D's.",
+    "Check out my huge **{thing}**. Isn't it gross?",
+    'Duh, **{thing}**.',
+    'Obviously **{thing}**.',
+    'Definitely **{thing}**.',
+    'Verily, I counsel thee for **{thing}**.',
+    '**{thing}** smothered in sausage gravy, with a side of hashbrowns, and slices of bacon.',
 ]
 
 NOT_ENOUGH_OPTIONS_PHRASE = 'I need to have at least two options!'
