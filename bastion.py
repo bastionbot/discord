3e2c28f063e95bc89827a05ab21a6bc1c7ed6528
bastionbot <droneship16@gmail.com>
2019-11-11 22:57:12 -0600

    Update bastion.py

--- bastion.py
+++ bastion.py
@@ -7,7 +7,7 @@ from bastion import init_bastion
 def main():
     logging.basicConfig(level=logging.WARN)
     config = ConfigParser()
-    config.read('config.ini')
+    config.read('/opt/bastion/config.ini')
 
     bastion = init_bastion(config)
 
