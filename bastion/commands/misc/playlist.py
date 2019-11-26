8dc78928637652944d65719e6efad38f50dace0a
bastionbot <droneship16@gmail.com>
2019-11-25 18:01:45 -0600

    Update playlist.py

--- bastion/commands/misc/playlist.py
+++ bastion/commands/misc/playlist.py
@@ -4,7 +4,7 @@ from configparser import ConfigParser
 async def build_playlist():
     config = ConfigParser()
     config.read('/opt/discord/config.ini')
-    channel = discord.get_channel(id=config['channel']['muzak'])
+    channel = discord.Client.get_channel(id=config['channel']['muzak'])
     links = ['youtube.com', 'youtu.be']
     videos = []
     async for message in channel.history():
