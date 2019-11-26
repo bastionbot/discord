21f6598c567f2066d2d5369b3741a7d06d691e2c
bastionbot <droneship16@gmail.com>
2019-11-25 19:09:34 -0600

    Update playlist.py

--- bastion/commands/misc/playlist.py
+++ bastion/commands/misc/playlist.py
@@ -1,13 +1,12 @@
 import re, asyncio
 
 async def build_playlist(self, ctx):
-    chan = int(ctx.bot.config['channel']['muzak'])
-    channel = ctx.bot.get_channel(chan)
+    channel = ctx.bot.get_channel(int(ctx.bot.config['channel']['muzak']))
     links = ['youtube.com', 'youtu.be']
     videos = []
     messages = await channel.history(limit=None).flatten()
     for message in messages:
         if any(link in message.content for link in links):
             videos.append(''.join(re.findall('(http[^\s]+)', message.content)))
-    print(videos)
-    return 'It\'s a work in progress. {0} links found.'.format(len(videos))
+    dedup = list(set(videos))
+    return 'It\'s a work in progress. {0} links found.'.format(len(dedup))
